b12d996837be77093f5294a0dc66ef47535086a9
Edward <huangxx_2155@qq.com>
2015-04-06 09:49:33 -0700

    add a overloaded crawl() function in Crawler.java file so that it is possible to support implicitly crawling 10-K file or explicitly crawling 10-K or 20-F

--- src/edu/cmu/sv/webcrawler/apis/CrawlerResource.java
+++ src/edu/cmu/sv/webcrawler/apis/CrawlerResource.java
@@ -12,6 +12,12 @@ import edu.cmu.sv.webcrawler.models.Crawler;
 @Path("/crawl")
 public class CrawlerResource {
 
+	/**
+	 * 
+	 * @param symbol
+	 * @param docType
+	 * @return this function will crawl the company with SIC symbol as symbol, docType as docType
+	 */
 	@GET
 	@Path("/{param}")
 	public Response crawlBySymbol(@PathParam("param") String symbol, @QueryParam("docType") String docType) {
@@ -20,16 +26,21 @@ public class CrawlerResource {
 		String fail_output = "Fail to crawl the risk factor for company with symbol " 
 							+ symbol + ",docType " + docType;
 		Crawler c = new Crawler();
-		if(c.crawl(symbol) < 0)
+		if(c.crawl(symbol,docType) < 0)
 			return Response.status(200).entity(fail_output).build();
 	
 		return Response.status(200).entity(output).build();
 	}
 
+	
+	/**
+	 * 
+	 * @return this function will crawl the 10-K document of all companies in records
+	 */
 	@GET
 	public Response crawlAll() {
 		String output = "Crawl the risk factors of all companies";
-		String fail_output = "Fail to crawl risk factor for company ";
+		String fail_output = "Fail to crawl risk factor for companies";
 		Crawler c = new Crawler();
 		Symbols ss = new Symbols();
 		for (String symbol : ss.getSymbols()) {
--- src/edu/cmu/sv/webcrawler/models/Crawler.java
+++ src/edu/cmu/sv/webcrawler/models/Crawler.java
@@ -12,7 +12,21 @@ public class Crawler {
 	 * 			-1 when Download10KbyCIK fail
 	 */
 	public int crawl(String symbol){
+		//should try to call different crawling function here
 		Get10K g=new Get10K();
 		return g.Download10KbyCIK(symbol, false);
+	}
+	
+	/**
+	 * 
+	 * @param symbol, 
+	 * 				the CIK symbol for certian company
+	 * @param docType,
+	 * 				the documentType being crawled
+	 * @return 1 when Download10KbyCIK success, 
+	 * 			-1 when Download10KbyCIK fail
+	 */
+	public int crawl(String symbol, String docType){
+		return 1;
 	}
 }
