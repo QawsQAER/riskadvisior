12bd97ab80d36ce9c001b56bbb1fc1af36f95447
mingqi830 <mqi@andrew.cmu.edu>
2015-04-07 14:46:53 -0700

    add frequency api

--- src/edu/cmu/sv/webcrawler/apis/CategoriesResource.java
+++ src/edu/cmu/sv/webcrawler/apis/CategoriesResource.java
@@ -32,6 +32,17 @@ public class CategoriesResource {
         Categories c = new Categories(map);
         return c.getMap();
     }
+    
+    @GET
+    @Path("/frequency/{param}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Integer> getCategoryBySymbolWithFrequency(
+            @PathParam("param") String symbol, @QueryParam("year") String year) {
+        Keywords ks = new Keywords();
+        Map<String, Integer> map = ks.getKeywordsFrequency(symbol, year);
+        Categories c = new Categories(map);
+        return c.getMap();
+    }
 
     @GET
     @Path("/addKeyword")
